84745bef07f88fdddf7c4db8ea922b6499406e1b
sowrimalaiyan.m <sowrimalaiyan.m@cannyfore.com>
2025-10-06 14:45:11 +0530

    main-update-for-render

--- BackEndApi/app/main.py
+++ BackEndApi/app/main.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     import os
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(
-        "main:app",
+        "app.main:app",
         host="0.0.0.0",
         port=port,
         reload=False,       # disable reload for production
