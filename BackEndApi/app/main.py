51ee9134653e1da93f45056bd045aace8e8c65aa
sowrimalaiyan.m <sowrimalaiyan.m@cannyfore.com>
2025-10-17 11:53:20 +0530

    render-deplly-changes-main

--- BackEndApi/app/main.py
+++ BackEndApi/app/main.py
@@ -24,13 +24,13 @@ app.add_middleware(
 app.include_router(testrouter.router, prefix="/api")
 app.include_router(fileupload_router, prefix="/api")
 
-if __name__ == "__main__":
-    import os
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=port,
-        reload=False,       # disable reload for production
-        log_level="info"    # useful for debugging
-    )
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.getenv("PORT", 8000))
+#     uvicorn.run(
+#         "app.main:app",
+#         host="0.0.0.0",
+#         port=port,
+#         reload=False,       # disable reload for production
+#         log_level="info"    # useful for debugging
+#     )
